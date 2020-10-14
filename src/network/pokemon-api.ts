637ab9b54aa584747782fa9e2aea6c5220946ff3
Kentaro Umetsu <umetsu.kentaro@adways.net>
2020-10-14 14:54:50 +0900

    :sparkles: Add pokemon api

---
+++ src/network/pokemon-api.ts
@@ -0,0 +1,45 @@
+import gql from "graphql-tag";
+import { print } from "graphql/language/printer";
+import { GetAllPokemonsQuery } from "../generated/graphql";
+
+export async function fetchAllPokemons(): Promise<GetAllPokemonsQuery> {
+  const query = gql`
+    query GetAllPokemons {
+      pokemons(first: 151) {
+        id
+        number
+        image
+        name
+        types
+        attacks {
+          fast {
+            name
+            type
+            damage
+          }
+        }
+        evolutions {
+          id
+          name
+          image
+          types
+        }
+      }
+    }
+  `;
+
+  const res = await fetch("https://graphql-pokemon2.vercel.app", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: print(query),
+    }),
+  });
+  const json = await res.json();
+  if (json.errors) {
+    throw new Error(json.errors.map((e: any) => e.message).join("\n"));
+  }
+  return json.data;
+}
